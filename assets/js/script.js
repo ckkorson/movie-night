6fce1f3df15fd31b6a72c9146a09e70a47b64c6f
Caleb Korson <caleb@Calebs-Air.attlocal.net>
2022-09-12 21:01:53 -0400

    added fetch from google api based on nytimes api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-function getApi() {
-    let requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=dragon+subject:fantasy'
+function getApi(isbn, bookName) {
+    let requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + bookName + 'isbn:' + isbn
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
@@ -10,15 +10,26 @@ function getApi() {
 }
 
 function getApi2() {
-    let requestUrl = 'https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=8DYkNsuedhfrGlFCoGJ82DgppVWLNTXD'
+    let listName = 'education'
+    let requestUrl = 'https://api.nytimes.com/svc/books/v3/lists/current/' + listName + '.json?api-key=8DYkNsuedhfrGlFCoGJ82DgppVWLNTXD'
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
       })
     .then(function(data) {
         console.log(data)
+        let x = getRandom(data)
+        let isbn = data.results.books[x].primary_isbn13
+        let bookName = data.results.books[x].title
+        console.log(x)
+        console.log(isbn)
+        console.log(bookName)
+        getApi(isbn, bookName)
     })
 }
 
-getApi()
+function getRandom(data) {
+    return Math.floor(Math.random() * data.results.books.length)
+ }
+
 getApi2()
