5c7f0110d247dac1a0e054a16a92657009966f15
Caleb Korson <caleb@Calebs-Air.attlocal.net>
2022-09-12 22:35:27 -0400

    added code to check user checkbox selections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 let categoryArr = ['fiction', 'nonfiction', 'graphic-books-manga', 'young-adult', 'business', 'crime',
 'science', 'sports', 'travel']
+nyTimesCategory = ['hardcover-fiction', 'hardcover-nonfiction', 'graphic-books-and-manga', 'young-adult',
+'business-books', 'crime-and-punishment', 'science', 'sports', 'travel']
 
 function googleBooksApi(isbn, bookName) {
     let requestUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + bookName + 'isbn:' + isbn
@@ -12,16 +14,17 @@ function googleBooksApi(isbn, bookName) {
     })
 } 
 
-function nyTimesApi() {
-    let listName = 'education'
-    let requestUrl = 'https://api.nytimes.com/svc/books/v3/lists/current/' + listName + '.json?api-key=8DYkNsuedhfrGlFCoGJ82DgppVWLNTXD'
+function nyTimesApi(categorySelections) {
+    let randomCategory = getRandomCategory(categorySelections)
+    console.log(categorySelections[randomCategory])
+    let requestUrl = 'https://api.nytimes.com/svc/books/v3/lists/current/' + categorySelections[randomCategory] + '.json?api-key=8DYkNsuedhfrGlFCoGJ82DgppVWLNTXD'
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
       })
     .then(function(data) {
         console.log(data)
-        let x = getRandom(data)
+        let x = getRandomBook(data)
         let isbn = data.results.books[x].primary_isbn13
         let bookName = data.results.books[x].title
         console.log(x)
@@ -31,12 +34,27 @@ function nyTimesApi() {
     })
 }
 
-function getRandom(data) {
+function getRandomBook(data) {
     return Math.floor(Math.random() * data.results.books.length)
  }
 
-//  function checkboxes() {
-//     for(let i = 0; i < categoryArr.length; i++)
-//  }
+ function getRandomCategory(categorySelections) {
+    return Math.floor(Math.random() * categorySelections.length)
+ }
 
-nyTimesApi()
+ function checkboxes() {
+    let categorySelections = []
+    for(let i = 0; i < categoryArr.length; i++) {
+        // if(document.getElementById(categoryArr[i]).checked)
+        let x = document.getElementById(categoryArr[i]).checked
+        if(x) {
+            categorySelections.push(nyTimesCategory[i])
+        }
+    }
+    console.log(categorySelections)
+    nyTimesApi(categorySelections)
+ }
+
+document.getElementById('submit').addEventListener('click', checkboxes)
+
+// nyTimesApi()
